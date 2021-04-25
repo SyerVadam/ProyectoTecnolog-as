b0234240296f30828a804a575f4c1d504a500dc4
Syer <israzavrivera@gmail.com>
2021-04-25 16:42:23 -0500

    Ventanas de Usuario creadas

---
+++ ProyectoFinalCorregido/AplicacionEscritorioDireccionGeneral/VentanaRegistrarUsuario.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AplicacionEscritorioDireccionGeneral
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaRegistrarUsuario.xaml
+    /// </summary>
+    public partial class VentanaRegistrarUsuario : Window
+    {
+        public VentanaRegistrarUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
